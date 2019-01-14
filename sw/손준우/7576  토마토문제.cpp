e8510f923d8e072967d2711a22bff89004f5fd30
thrk941106 <35682225+thrk941106@users.noreply.github.com>
2019-01-14 21:56:48 +0900

    solve by bfs // refactoring 100ms -> 80ms

--- sw/손준우/7576  토마토문제.cpp
+++ sw/손준우/7576  토마토문제.cpp
@@ -5,15 +5,14 @@
 using namespace std;
 int n,m;
 int a[1001][1001];
-int d[1001][1001];
 vector<pair<int,int> > v;
 int dx[] = {1,-1,0,0};
 int dy[] = {0,0,1,-1};
 queue<pair<int,int>> q;
 int cnt;
-int MAX=0;
+int MAX=1;
 
-void bfs() {
+int bfs() {
 	int x, y;
     while(!q.empty()){
         x = q.front().first;
@@ -22,17 +21,24 @@ void bfs() {
         for(int k=0 ; k<4 ; k++){
             int nx = x+dx[k], ny=y+dy[k];
             if(0<nx && 0<ny && nx<=n && ny <= m){
-                if(a[nx][ny] == 0 && d[nx][ny]==0){
+                if(a[nx][ny]==0){
                     q.push({nx,ny});
-                    d[nx][ny] = d[x][y]+1;
-                    if(MAX < d[nx][ny]){
-                        MAX = d[nx][ny];   
+                    a[nx][ny] = a[x][y]+1;
+                    if(MAX < a[nx][ny]){
+                        MAX = a[nx][ny];   
                     }
-                    a[nx][ny] = 1;
                 }
             }
         }   
     }
+    for(int i=1 ; i<=n ; i++){
+        for(int j=1 ; j<=m ; j++){
+            if(a[i][j]==0){
+        		return -1;
+        	}
+        }
+    }
+    return MAX-1;
 }
 
 int main(){
@@ -47,19 +53,10 @@ int main(){
             if(a[i][j]==1){
                 q.push({i,j});
             }
-            d[i][j] = 0;
         }
     }
-    bfs();
 
-    for(int i=1 ; i<=n ; i++){
-        for(int j=1 ; j<=m ; j++){
-            if(a[i][j]==0){
-        		cout << -1;
-        		return 0;
-        	}
-        }
-    }
-    cout << MAX;
+
+    cout <<  bfs();
     
 }
