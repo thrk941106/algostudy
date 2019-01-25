bbeeb9ca3fc76ec654c17efaad6ca45f8f9fd572
thrk941106 <35682225+thrk941106@users.noreply.github.com>
2019-01-25 16:29:41 +0900

    solve by dp

---
+++ sw/손준우/16194 카드구매하기2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int N;
+int p[1001];
+int a[1001];
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> N;
+    for(int i=1; i<=N ; i++){
+        cin >> p[i];
+    }
+    
+    for(int i=1; i<=N ; i++){
+        for(int j=1 ; j<=i ; j++){
+            if(a[i]==0){
+                 a[i]=p[j]+a[i-j];
+            }
+            else if(a[i] > p[j]+a[i-j]){
+                a[i]=p[j]+a[i-j];
+            }
+        }
+    }
+    cout << a[N];
+    
+
+}
